beed75d5d33f95b51c879bb865c7458af2f9c55d
Olga <dochnohi@yandex.ru>
2023-01-27 18:56:36 +0300

    переделала 4.3

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,14 +1,9 @@
 -- liquibase formatted sql
 
 
--- changeset oKrivobokova:1
+--changeset oKrivobokova:3
 
-CREATE INDEX student_name_index ON student (name);
-
--- changeset oKrivobokova:2
-
-CREATE INDEX faculty_name_color_index ON faculty (name, color);
-
+DROP INDEX student_name_index;
 
 
 
